dce009d4c0ba098e573f15dd0858a218595d951a
flis_ann <ann.flismark@gmail.com>
2021-02-03 16:18:10 +0100

    Adding controller and model for comment

--- spec/requests/api/article/visitor_can_leave_a_comment_spec.rb
+++ spec/requests/api/article/visitor_can_leave_a_comment_spec.rb
@@ -1,1 +1,17 @@
-RSPEC
+RSpec.describe 'POST api/posts', type: :request do
+ 
+  describe 'successfully leave a comment' do
+    before do
+      post "/api/comment",
+      params: {
+        comment: {
+          content: "Cool cat",
+        }
+      }
+    end
+
+    it 'is expected to return a 201 status' do
+      expect(response).to have_http_status 201
+    end
+  end
+end
